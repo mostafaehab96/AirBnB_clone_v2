15ebb7db9d4592d096394867fdc87e785bed3768
mostafaehab96 <mostafa96ehab@gmail.com>
2023-06-20 22:02:16 +0000

    Remove the rest of the classes again and keep City, State

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -21,7 +21,7 @@ class DBStorage:
     """interaacts with the MySQL database"""
     __engine = None
     __session = None
-    classes = [City, State, Place, Review, User, Amenity]
+    classes = [City, State]
 
     def __init__(self):
         """Instantiate a DBStorage object"""
