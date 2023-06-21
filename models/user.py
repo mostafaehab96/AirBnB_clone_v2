6e4be4915ad513e2ad2ef94cd4e4e7036c331aab
mostafaehab96 <mostafa96ehab@gmail.com>
2023-06-21 16:29:36 +0000

    Make the __tablename__ out of if

--- models/user.py
+++ models/user.py
@@ -8,8 +8,9 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = "users"
+
     if models.storage_t == "db":
-        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
