b3eaf032b026dc958b1f30a16d2da9892a5da6d5
mostafaehab96 <mostafa96ehab@gmail.com>
2023-06-21 16:33:37 +0000

    Make __tablename__ out of if in place.py

--- models/place.py
+++ models/place.py
@@ -7,8 +7,8 @@ from sqlalchemy import String, Column, ForeignKey, Integer, Float
 
 class Place(BaseModel, Base):
     """ A place to stay """
+    __tablename__ = "places"
     if models.storage_t == "db":
-        __tablename__ = "places"
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
