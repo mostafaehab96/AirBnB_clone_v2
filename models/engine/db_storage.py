e89db887ef7b1964555880d62ce5bf4f9d97d039
mostafaehab96 <mostafa96ehab@gmail.com>
2023-06-21 16:12:07 +0000

    Add option pool_pre_ping in create_engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -21,7 +21,7 @@ class DBStorage:
     """interaacts with the MySQL database"""
     __engine = None
     __session = None
-    classes = [City, State]
+    classes = [City, State, User, Place]
 
     def __init__(self):
         """Instantiate a DBStorage object"""
@@ -34,7 +34,9 @@ class DBStorage:
                                       format(HBNB_MYSQL_USER,
                                              HBNB_MYSQL_PWD,
                                              HBNB_MYSQL_HOST,
-                                             HBNB_MYSQL_DB))
+                                             HBNB_MYSQL_DB),
+                                      pool_pre_ping=True
+                                      )
         if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
