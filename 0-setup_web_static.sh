9ad7b63b29321ee99861a140f596ba4346c94847
mostafaehab96 <mostafa96ehab@gmail.com>
2023-07-07 12:12:05 +0000

    Change the ownership after creating the symlink

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -24,15 +24,13 @@ if ! command -v nginx &> /dev/null; then
     sudo apt-get -y update
     sudo apt-get -y install nginx
 fi
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo chown -R ubuntu:ubuntu /data/
 sudo chown -R ubuntu:ubuntu /etc/nginx/sites-available/default
 echo "$my_html" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/default_server/d' /etc/nginx/sites-available/default
 sudo echo "$my_configs" >> /etc/nginx/sites-available/default
 sudo service nginx restart
