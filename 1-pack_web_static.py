920b16f926aecbecdea9dff64f73b6511cb9c4e8
mostafaehab96 <mostafa96ehab@gmail.com>
2023-07-07 02:56:30 +0000

    Add files 1-pack_web_static.py 2-do_deploy_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Fabric script that generates a .tgz archive from
+the contents of the web_static.
+"""
+
+def do_pack():
+    """The function that do what's
+    described a bove."""
+    pass
---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers.
+"""
+
+def do_deploy(archive_path):
+    """Returns True if all operations have been done correctly,
+    otherwise returns False.
+    """
+    return False
