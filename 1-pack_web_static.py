ef68142492550ebcee93b6435da3b0265a63c166
mostafaehab96 <mostafa96ehab@gmail.com>
2023-07-08 14:24:47 -0700

    Add task 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
-""" Fabric script that generates a .tgz archive from
-the contents of the web_static.
-"""
+"""Defines a function that compresses the web_static folder using fabric."""
+
+from fabric.api import local
+from datetime import datetime
+
 
 def do_pack():
-    """The function that do what's
-    described a bove."""
-    pass
+    """Generates a tgz archive of web_static folder
+    in a version folder.
+    Return: the path of the archive if generated or None otherwise
+    """
+    time = datetime.now()  # For renaming the file
+    time = time.strftime("%Y%m%d%H%M%S")
+    filename = f"web_static_{time}.tgz"
+
+    local('mkdir -p versions')  # Make the file version if not exist
+    result = local(f'tar -czvf versions/{filename} web_static')
+
+    if result.succeeded:
+        return f"versions/{filename}"
+    else:
+        None
