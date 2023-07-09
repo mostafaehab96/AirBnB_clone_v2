3b527a62c6afa555c732297603e40a0d2812330c
mostafaehab96 <mostafa96ehab@gmail.com>
2023-07-08 17:48:48 -0700

    Add task 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,9 +3,36 @@
 """Fabric script (based on the file 1-pack_web_static.py)
 that distributes an archive to your web servers.
 """
+from fabric.api import run, env, put
+from os import path
+
+
+env.hosts = ['52.201.157.19', '100.26.230.29']
+env.user = "ubuntu"
+
 
 def do_deploy(archive_path):
     """Returns True if all operations have been done correctly,
     otherwise returns False.
     """
-    return False
+    if archive_path is None or not path.exists(archive_path):
+        return False
+
+    filename = archive_path.split('/')[1]  # Filename
+    just_name = filename.split('.')[0]  # Filename without extension
+    destpath = "/data/web_static/releases/{}".format(just_name)
+
+    try:
+        put(archive_path, '/tmp/')
+        run("mkdir -p {}".format(destpath))
+        run("tar xzf /tmp/{} -C {}".format(filename, destpath))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}/web_static/* {}/".format(destpath, destpath))
+        run("rm -rf {}/web_static".format(destpath))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(destpath))
+    except Exception:
+        return False
+
+    print("New version deployed!")
+    return True
