c107d107bf9234a213a58af91bad6bdd403a263a
mostafaehab96 <mostafa96ehab@gmail.com>
2023-07-21 23:15:37 +0000

    Add task 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""The second flask task just returns Hello HBNB! in /
+and HBNB in /hbnb and so on...
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_fun(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_cool(text="is cool"):
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
